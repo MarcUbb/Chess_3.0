371af9539c2936f09e6b4eacd300e45644d74a0a
MarcUbb <marcubb@outlook.de>
2020-06-04 19:32:52 +0200

    Update moves.py
    human_move()

--- moves.py
+++ moves.py
@@ -9,7 +9,7 @@ import copy
 
 def ai_move():
     if constants.GAMEMODE == 1 and constants.TURN != constants.AI_TURN: # looks if its AIs turn (only for gamemode 1)
-        return 0                                                        #TODO can't that just be a "continue"?
+        return                                                          #TODO can't that just be a "continue"?
     pygame.time.wait(constants.AI_DELAY)                                # short delay so AI moves become visible
     AI_MESSAGES = [0]
     if constants.TURN == 1 or (constants.GAMEMODE == 1 and constants.TURN == constants.AI_TURN):                                # AI which is used in gamemode 1
@@ -24,35 +24,41 @@ def ai_move():
     game_mechanics.check_for_chess()                                                # checks if somebody is in check or lost
     constants.BOARD = game_mechanics.choose_figure(constants.BOARD, AI_MESSAGES[0]) # AI chooses figure if pawn reaches end of board
     constants.MOVES += 1
-    constants.TURN = 1 if constants.TURN == 2 else 2                                                            # counts how many moves have been done
+    basic_functions.change_turn()                                                         # counts how many moves have been done
 
 
 
 def human_move():
-    basic_functions.draw_window()
+    #basic_functions.draw_window()
 
     if pygame.mouse.get_pressed()[0]:
         game_mechanics.reset_markers()
-        if not constants.MOVE:
+        if constants.MOVE == False:
             constants.FIELD_CHOSEN = constants.BOARD.fields[basic_functions.mouse_coordinates_to_field()[1]][basic_functions.mouse_coordinates_to_field()[0]]  # checks which coordinates were clicked and converts them in field coordinates
             constants.FIELD_CHOSEN.active = True  # activates field to be marked
             if constants.FIELD_CHOSEN.figure_team == constants.TURN:  # checks if figure on chosen field belongs to the team whichs turn it is
                 constants.MOVE = True  # toggles MOVE-MODE so next click will be either a move or chosing another friendly figure
-        if constants.MOVE:
+                game_mechanics.mark_move_pattern(constants.FIELD_CHOSEN, constants.BOARD)
+        else:
             game_mechanics.mark_move_pattern(constants.FIELD_CHOSEN, constants.BOARD)
             constants.FIELD_CHOSEN.active = True
             constants.FIELD_MOVE = constants.BOARD.fields[basic_functions.mouse_coordinates_to_field()[1]][basic_functions.mouse_coordinates_to_field()[0]]  # checks which coordinates were clicked and converts them in field coordinates
-            if constants.FIELD_CHOSEN == constants.FIELD_MOVE:
-                return
-            elif not constants.FIELD_MOVE.active:
-                constants.FIELD_CHOSEN = constants.BOARD.fields[basic_functions.mouse_coordinates_to_field()[1]][basic_functions.mouse_coordinates_to_field()[0]]
-                return
-            elif constants.FIELD_MOVE.active:
+            if constants.FIELD_MOVE.figure_team != constants.TURN and constants.FIELD_MOVE.active: 
                 game_mechanics.check_for_draw()
                 game_mechanics.valid_ai_move(constants.FIELD_CHOSEN, constants.FIELD_MOVE)
                 game_mechanics.move_dead_figures(constants.FIELD_MOVE)
                 game_mechanics.replace_figure(constants.FIELD_CHOSEN, constants.FIELD_MOVE)
                 game_mechanics.check_for_chess()
                 constants.BOARD = game_mechanics.choose_figure(constants.BOARD, 0)
-                constants.TURN = 1 if constants.TURN == 2 else 2
+                basic_functions.change_turn()  
                 constants.MOVE = False
+
+            else:                                                                                               #if a friendly figure is selected
+                game_mechanics.reset_markers()                                                                            #resets move pattern of previously picked figure
+                constants.FIELD_CHOSEN = constants.BOARD.fields[basic_functions.mouse_coordinates_to_field()[1]][basic_functions.mouse_coordinates_to_field()[0]]   #checks which coordinates were clicked and converts them in field coordinates
+                constants.FIELD_CHOSEN.active = True                                                                      #activates field to be marked
+                if constants.FIELD_CHOSEN.figure_team == constants.TURN:                                                            #checks if figure on chosen field belongs to the team whichs turn it is
+                    game_mechanics.mark_move_pattern(constants.FIELD_CHOSEN,constants.BOARD)                                                       #shows possible moves for the chosen figure
+                    constants.MOVE = True                                                                                 #toggles MOVE-MODE so next click will be either a move or chosing another friendly figure
+                else:
+                    constants.MOVE = False
